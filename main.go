b8c3f1007e9dbebc141ecc3ec1f539dd927aee29
adhyayana108 <adhyayana108@gmail.com>
2026-07-15 23:55:27 +0530

    middleware

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// middleware
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := newResponseWriter(w)
+		next.ServeHTTP(rw, r)
+		duration := time.Since(start)
+		log.Printf("%s %s %s %d %s", r.Method, r.RemoteAddr, r.URL.Path, rw.statusCode, duration)
+	})
+}
